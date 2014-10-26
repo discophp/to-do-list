388f26dd64a3eaf5d57635770ade4171d9381d8c
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-10-26 19:00:39 -0400

    include create table

--- app/model/user.model.php
+++ app/model/user.model.php
@@ -6,6 +6,23 @@ Class user extends Disco\classes\Model {
     public $ids = 'id';
 
 
+    public function createTable(){
+        return DB::query('
+            CREATE TABLE `user` (
+              `id` int(11) NOT NULL AUTO_INCREMENT,
+              `email` varchar(120) NOT NULL,
+              `password` varchar(180) NOT NULL,
+              `created` datetime NOT NULL,
+              `last_access` datetime DEFAULT NULL,
+              `verified` tinyint(1) DEFAULT 0,
+              `admin` tinyint(1) DEFAULT 0,
+              PRIMARY KEY (`id`)
+          )'
+        );
+
+    }//createTable
+
+
     public function signup($data){
         $data['password'] = Crypt::hash($data['password']);
 
